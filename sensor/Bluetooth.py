441db691da147c99ed193924fd27e534a5259613
newstarfish@163.com <980206yxx>
2018-06-01 20:32:23 +0800

    蓝牙定位模块代码更新 1.蓝牙RSSI计算距离 2.定位算法

--- sensor/Bluetooth.py
+++ sensor/Bluetooth.py
@@ -7,3 +7,15 @@ class Bluetooth:
         """初始化蓝牙模块"""
         pass
 
+    def fixed_point(self,xc,ya,yc,r1,r2,r3):
+        """定位计算模块    xa  xb  yb  均为0"""
+        yd=(r1**2-r3**2+ya**2)/2*ya
+        xd=(r1**2-r2**2+xc**2+yc**2-2*yd*yc)/2*xc
+        return xd,yd
+
+    def RSSI_distance(self,rssi,A,N):
+        """蓝牙RSSI计算距离"""
+        d=10**((abs(rssi)-A)/10*N)
+        """ A为发射端和接收端相隔一米时的信号强度
+            N为环境衰减因子"""
+        return d
