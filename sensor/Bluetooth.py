87d5b65ed4c289ee2a47b3ffc8969c6896e9d1a4
newstarfish@163.com <980206yxx>
2018-06-02 15:22:39 +0800

    蓝牙定位模块代码更新 1.坐标数据整合 2.直角坐标系绘图 3.部分代码及注释更新

--- sensor/Bluetooth.py
+++ sensor/Bluetooth.py
@@ -1,5 +1,6 @@
 """蓝牙模块"""
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Bluetooth:
 
@@ -8,14 +9,39 @@ class Bluetooth:
         pass
 
     def fixed_point(self,xc,ya,yc,r1,r2,r3):
-        """定位计算模块，好奇下面俩式子从哪儿来的，在我草稿本上，(＾－＾)V，xa  xb  yb  均为0"""
-        yd=(r1**2-r3**2+ya**2)/2*ya
-        xd=(r1**2-r2**2+xc**2+yc**2-2*yd*yc)/2*xc
+        """定位计算模块，好奇下面俩式子从哪儿来的，在我草稿本上，(＾－＾)V，xa  xb  yb  均为0
+        r1为BD距离，r2为DC距离，r3为AD距离"""
+        yd = (r1**2-r3**2+ya**2)/2*ya
+        xd = (r1**2-r2**2+xc**2+yc**2-2*yd*yc)/2*xc
         return xd,yd
-
     def RSSI_distance(self,rssi,A,N):
         """蓝牙RSSI计算距离"""
-        d=10**((abs(rssi)-A)/10*N)
+        d = 10**((abs(rssi)-A)/10*N)
         """ A为发射端和接收端相隔一米时的信号强度
             N为环境衰减因子"""
         return d
+    def CSYS (self,xd,yd,xc,ya,yc):
+        """直角坐标系建系及绘图"""
+        plt.plot(xc, yc, 'or-')
+        plt.plot(0, 0, 'or-')
+        plt.plot(0, ya, 'or-')
+        plt.figure()
+        plt.plot(xd, yd,'xb-')
+        plt.show()
+    def coordinate_system_data (self,distance):
+        """探测坐标数据归类"""
+        global xd
+        global yd
+        xd.append(distance[0])
+        yd.append(distance[1])
+
+    A = int(input("请输发射端和接收端相隔一米时的信号强度"))
+    N = int(input("环境衰减因子"))
+    xc = int(input("请输入xc的值"))
+    yc = int(input("请输入yc的值"))
+    ya = int(input("请输入ya的值"))
+    xd = []
+    yd = []
+    distance = fixed_point(xc,ya,yc,r1,r2,r3)
+    coordinate_system_data(distance)
+    CSYS(xd, yd, xc, ya, yc)
