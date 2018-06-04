b26dd4d29aa20bf327f3130013e3ad71f9f8e59a
newstarfish@163.com <980206yxx>
2018-06-04 23:15:02 +0800

    蓝牙定位模块代码更新 1.更新蓝牙数据读取 2.更改部分注释 3.安装第三方工具包 beacontools,matplotlib

--- sensor/Bluetooth.py
+++ sensor/Bluetooth.py
@@ -1,16 +1,15 @@
 """蓝牙模块"""
-import numpy as np
 import matplotlib.pyplot as plt
+import time
+from beacontools import BeaconScanner
 
 class Bluetooth:
 
     def __init__(self):
         """初始化蓝牙模块"""
         pass
-
     def fixed_point(self,xc,ya,yc,r1,r2,r3):
-        """定位计算模块，好奇下面俩式子从哪儿来的，在我草稿本上，(＾－＾)V，xa  xb  yb  均为0
-        r1为BD距离，r2为DC距离，r3为AD距离"""
+        """定位计算模块，均为0,r1为BD距离，r2为DC距离，r3为AD距离"""
         yd = (r1**2-r3**2+ya**2)/2*ya
         xd = (r1**2-r2**2+xc**2+yc**2-2*yd*yc)/2*xc
         return xd,yd
@@ -34,14 +33,33 @@ class Bluetooth:
         global yd
         xd.append(distance[0])
         yd.append(distance[1])
+    def bluetooch_data(self):
+        """蓝牙模块初始化传入参数"""
+        global data
+        data = {}
+        def callback(bt_addr, rssi, packet, additional_info):
+            data[bt_addr] = rssi
+        #   print ("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+        # scan for all iBeacon advertisements from beacons with the specified uuid
+        while (1):
+            scanner = BeaconScanner(callback)
+            scanner.start()
+            time.sleep(3)#睡眠时间自定义
+            scanner.stop()
+            return data
 
-    A = int(input("请输发射端和接收端相隔一米时的信号强度"))
-    N = int(input("环境衰减因子"))
+
+    A = int(input("请输发射端和接收端相隔一米时的信号强度，建议程序内部设置"))
+    N = int(input("环境衰减因子,建议程序内部设置值"))
     xc = int(input("请输入xc的值"))
     yc = int(input("请输入yc的值"))
     ya = int(input("请输入ya的值"))
     xd = []
     yd = []
+    data = bluetooch_data()
+    RSSIa = data[Amac]#Amac待定
+    RSSIb = data[Bmac]#Bmac待定
+    RSSIc = data[Cmac]#Cmac待定
     distance = fixed_point(xc,ya,yc,r1,r2,r3)
     coordinate_system_data(distance)
     CSYS(xd, yd, xc, ya, yc)
