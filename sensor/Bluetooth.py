840c68bc37a57df36d08c62248033bc0b90baab6
newstarfish@163.com <980206yxx>
2018-06-06 16:26:34 +0800

    蓝牙定位模块代码更新 1.错误更新

--- sensor/Bluetooth.py
+++ sensor/Bluetooth.py
@@ -3,27 +3,27 @@ import matplotlib.pyplot as plt
 import time
 import math
 from beacontools import BeaconScanner
-class Bluetooth:
+class Bluetooth():
     def __init__(self):
         """初始化蓝牙模块"""
-        A = int(input("请输发射端和接收端相隔一米时的信号强度，建议程序内部设置"))
-        N = int(input("环境衰减因子,建议程序内部设置值"))
-        xc = int(input("请输入xc的值"))
-        yc = int(input("请输入yc的值"))
-        ya = int(input("请输入ya的值"))
+        self.A = int(input("请输发射端和接收端相隔一米时的信号强度，建议程序内部设置"))
+        self.N = int(input("环境衰减因子,建议程序内部设置值"))
+        self.xc = int(input("请输入xc的值"))
+        self.yc = int(input("请输入yc的值"))
+        self.ya = int(input("请输入ya的值"))
 
     def fixed_point(self,xc,ya,yc,r1,r2,r3):
         """定位计算模块,r1为BD距离，r2为DC距离，r3为AD距离"""
         yd = (r1**2-r3**2+ya**2)/2*ya
         xd = (r1**2-r2**2+xc**2+yc**2-2*yd*yc)/2*xc
-        return xd,yd
+        return xd, yd
 
     def RSSI_distance(self,rssi,A,N):
         """蓝牙RSSI计算距离"""
-        d = 10**((abs(rssi)-A)/10*N)
+        r = 10**((abs(rssi)-A)/10*N)
         """ A为发射端和接收端相隔一米时的信号强度
             N为环境衰减因子"""
-        return d
+        return r
 
     def CSYS (self,xd,yd,xc,ya,yc):
         """直角坐标系建系及绘图，xd,yd 可以直接传数组"""
@@ -40,14 +40,16 @@ class Bluetooth:
         global yd
         xd.append(distance[0])
         yd.append(distance[1])
+        self.CSYS(xd,yd,self.xc,self.ya,self.yc)
+
     def bluetooch_data(self):
         """蓝牙模块初始化传入参数"""
-        #global data
-        #data = {}
+        global data
+        data = {}
+
         def callback(bt_addr, rssi, packet, additional_info):
-        data[bt_addr] = rssi
-        #print ("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-        # scan for all iBeacon advertisements from beacons with the specified uuid
+            print ("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+            # scan for all iBeacon advertisements from beacons with the specified uuid
 
         while (1):
             scanner = BeaconScanner(callback)
@@ -75,7 +77,7 @@ class Bluetooth:
         return gaussion_ave
 
     def add(self):
-        data = bluetooch_data()
+        data = self.bluetooch_data()
         RSSIa = []
         RSSIb = []
         RSSIc = []
@@ -89,16 +91,16 @@ class Bluetooth:
                 else:
                     RSSIc.append(rssi)
         if len(RSSIa)==20 and len(RSSIb)==20 and len(RSSIc)==20 :
-            scanner.stop()
-        ra = Gaussion_filter(RSSIa)
-        rb = Gaussion_filter(RSSIb)
-        rc = Gaussion_filter(RSSIc)
-        RSSI_distance(ra, 47, 1.7)
-        RSSI_distance(rb, 47, 1.7)
-        RSSI_distance(rc, 47, 1.7)
+            self.scanner.stop()
+        ra = self.Gaussion_filter(RSSIa)
+        rb = self.Gaussion_filter(RSSIb)
+        rc = self.Gaussion_filter(RSSIc)
+        r3 = self.RSSI_distance(ra, 47, 1.7)
+        r1 = self.RSSI_distance(rb, 47, 1.7)
+        r2 = self.RSSI_distance(rc, 47, 1.7)
+        coordinate_D = self.fixed_point(self.xc,self.ya,self.yc,r1,r2,r3)
+        self.coordinate_system_data(coordinate_D)
+
 
     xd = []
     yd = []
-    distance = fixed_point(xc,ya,yc,r1,r2,r3)
-    coordinate_system_data(distance)
-    CSYS(xd, yd, xc, ya, yc)
